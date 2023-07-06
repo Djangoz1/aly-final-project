53d0f86393443c07330340aab33cf18d62bf50f9
Djangoz1 <jg.djangone@gmail.com>
2023-07-06 19:08:29 +0200

    init lens hub

---
+++ frontend/src/app/community/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout } from "sections/Layout";
+
+const Community = () => {
+  return (
+    <Layout>
+      <div>Community</div>
+    </Layout>
+  );
+};
+
+export default Community;
