865f97102c6295e1b3feeb3730f62be460396758
Djangoz1 <jg.djangone@gmail.com>
2023-12-15 15:03:27 +0100

    fix some bug for config file

--- frontend/src/constants/config.js
+++ frontend/src/constants/config.js
@@ -1,35 +1,33 @@
 import testnetAddresses from "testnetaddresses.json";
-import addresses from "../../../backend/addresses.json";
-import testnetabis from "testnetabis.json";
+
+import addresses from "../../address.json";
+
+import testnetabis from "../../abistestnet.json";
+import abis from "../../abis.json";
 import { hardhat, sepolia } from "wagmi/chains";
 import { hardhat as _hardhat, sepolia as _sepolia } from "viem/chains";
 
-import abi_addr_s from "../../../backend/artifacts/contracts/system/AddressSystem.sol/AddressSystem.json";
-import abi_api_g from "../../../backend/artifacts/contracts/system/APIGet.sol/APIGet.json";
-import abi_cvh from "../../../backend/artifacts/contracts/cv/CVsHub.sol/CVsHub.json";
-import abi_api_p from "../../../backend/artifacts/contracts/system/APIPost.sol/APIPost.json";
-import abi_api_p_p from "../../../backend/artifacts/contracts/system/APIPostPayable.sol/APIPostPayable.json";
-import abi_bh from "../../../backend/artifacts/contracts/system/BalancesHub.sol/BalancesHub.json";
-import abi_l from "../../../backend/artifacts/contracts/launchpads/Launchpad.sol/Launchpad.json";
-import abi_erc20 from "../../../backend/artifacts/@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol/IERC20Metadata.json";
+export const CONFIG = "TESTNET"; // "Testnet";
+export const ABI_ADDRESS_SYSTEM = {
+  LOCAL: abis.addressSystem,
+  TESTNET: testnetabis.addressSystem,
+}[CONFIG];
+export const ABI_API_G = { LOCAL: abis.apiGet, TESTNET: testnetabis.apiGet }[
+  CONFIG
+];
+export const ABI_API_P = { LOCAL: abis.apiPost, TESTNET: testnetabis.apiPost }[
+  CONFIG
+];
+export const ABI_API_P_P = {
+  LOCAL: abis.apiPostPayable,
+  TESTNET: testnetabis.apiPostPayable,
+}[CONFIG];
 
-export const CONFIG = "LOCAL"; // "Testnet";
-export const ABI_ADDRESS_SYSTEM = abi_addr_s.abi;
-export const ABI_API_G = abi_api_g.abi;
-export const ABI_API_P = abi_api_p.abi;
-export const ABI_API_P_P = abi_api_p_p.abi;
 export const configs = {
   LOCAL: {
     pocketURL: "http://127.0.0.1:8090",
     addresses: addresses,
-    abis: {
-      cvsHub: abi_cvh.abi,
-      balancesHub: abi_bh.abi,
-      apiGet: abi_api_g.abi,
-      launchpad: abi_l.abi,
-      apiPostPayable: abi_api_p_p.abi,
-      erc20: abi_erc20.abi,
-    },
+    abis: abis,
     chain: [hardhat],
     viemChain: _hardhat,
   },
